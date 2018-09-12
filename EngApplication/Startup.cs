b313f6b86d3f22743d38404b4697dd2f7712e079
phamngocduy <phmngcduy@gmail.com>
2018-09-12 16:18:59 +0700

    Empty project

---
+++ EngApplication/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(EngApplication.Startup))]
+namespace EngApplication
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
